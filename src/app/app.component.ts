b4a7e7916849195cc3482656a7e1948f32260365
Khanh Gia Nguyen <ngiakhanh96@gmail.com>
2021-09-14 18:20:19 +0700

    Finish?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export interface Option {
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
@@ -21,27 +21,27 @@ export class AppComponent {
     id: 'Q1',
     options: [
       { optionId: 'O1-1', pointToQuestion: 'Q2' },
-      { optionId: 'O1-2', pointToQuestion: 'Q3' }
-    ]
+      { optionId: 'O1-2', pointToQuestion: 'Q3' },
+    ],
   };
   question2 = <Question>{
     id: 'Q2',
-    options: [{ optionId: 'O2-1', pointToQuestion: '04' }]
+    options: [{ optionId: 'O2-1', pointToQuestion: 'Q4' }],
   };
   question3 = <Question>{
     id: 'Q3',
-    options: [{ optionId: 'O3-1', pointToQuestion: null }]
+    options: [{ optionId: 'O3-1', pointToQuestion: null }],
   };
   question4 = <Question>{
     id: 'Q4',
-    options: [{ optionId: 'O4-1', pointToQuestion: null }]
+    options: [{ optionId: 'O4-1', pointToQuestion: null }],
   };
 
   questionDict = {
     ['Q1']: this.question1,
     ['Q2']: this.question2,
     ['Q3']: this.question3,
-    ['Q4']: this.question4
+    ['Q4']: this.question4,
   };
 
   possibleResults: string[][] = [];
@@ -52,17 +52,19 @@ export class AppComponent {
   }
 
   calculatePossibleResults(entryQuestion: Question) {
-    entryQuestion.options.forEach(opt => this.calculateCurrentResult(opt));
+    entryQuestion.options.forEach((opt) =>
+      this.calculateCurrentResult(opt, [entryQuestion.id])
+    );
   }
 
   calculateCurrentResult(option: Option, currentResult: string[] = []) {
-    if (option.pointToQuestion) {
+    if (!option.pointToQuestion) {
       this.possibleResults.push(currentResult);
     } else {
       const newCurrentResult = [...currentResult];
       newCurrentResult.push(option.pointToQuestion);
       const nextQuestion = this.questionDict[option.pointToQuestion];
-      nextQuestion.options.forEach(opt =>
+      nextQuestion.options.forEach((opt) =>
         this.calculateCurrentResult(opt, newCurrentResult)
       );
     }
